6b456656be770e52174310965662229282acb066
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-05-24 20:33:57 +0530

    creating components and linking

---
+++ listly/components/Navbar.js
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Lists</h1>
+      </div>
+
+      <Link href="/"><a>Home</a></Link>
+
+      <Link href="/about"><a>About</a></Link>
+
+      <Link href="/lists"><a>Listing</a></Link>
+      
+      <Link href="/lists/test"><a>test</a></Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- listly/pages/index.js
+++ listly/pages/index.js
@@ -1,11 +1,21 @@
+import Link from "next/link";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Initial setup</h1>
 
       <p>lorem ipsum dolor sit amet, consectetur adipis text</p>
 
       <p>lorem ipsum dolor sit amet, consectetur adipis text</p>
+
+      <Link href="/lists">
+        <a>See listing </a>
+      </Link>
+      <Footer />
     </div>
   );
 }
