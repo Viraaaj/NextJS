b792245714f348a1e49eee7108cb6b3e0befdea3
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-05-27 19:54:48 +0530

    Dynamic routes getStaticPaths

--- listly/pages/lists/[id].js
+++ listly/pages/lists/[id].js
@@ -1,11 +1,26 @@
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  const paths = data.map((person) => {
+    return {
+      params: { id: person.id.toString() },
+    };
+  });
+
+  return {
+    // paths: paths
+    paths, //we can use like this also cause names are same
+    fallback: false
+  };
+};
+
 const Details = () => {
-    return ( 
-        <div>
-            <h1>
-                Details
-            </h1>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <h1>Details</h1>
+    </div>
+  );
+};
+
 export default Details;
